84901679c35830f6efb2c3a00872cb0319ced7d8
yslolly <anneleen.tijtgat@outlook.com>
2022-07-02 17:09:47 +0200

    first commit

---
+++ 2d Arrays/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _2d_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] numberGrid = {
+                {1,2 },
+                {3,4 },
+                {5,6 }
+            };
+            int[,] myArray = new int[2, 3]; 
+
+            Console.WriteLine(numberGrid[1,1]);
+
+            Console.ReadLine();
+        }
+    }
+}
